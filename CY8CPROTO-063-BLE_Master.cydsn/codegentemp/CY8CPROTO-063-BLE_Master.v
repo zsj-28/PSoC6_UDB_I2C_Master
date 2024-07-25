8f1411d861cadf35c24507999d2b10ccb90a4352
Thomas Li <tyli@andrew.cmu.edu>
2024-07-25 15:57:19 -0400

    project.h rebuild succeeded

--- CY8CPROTO-063-BLE_Master.cydsn/codegentemp/CY8CPROTO-063-BLE_Master.v
+++ CY8CPROTO-063-BLE_Master.cydsn/codegentemp/CY8CPROTO-063-BLE_Master.v
@@ -1,6 +1,6 @@
 // ======================================================================
 // CY8CPROTO-063-BLE_Master.v generated from TopDesign.cysch
-// 07/24/2024 at 18:56
+// 07/25/2024 at 15:55
 // This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
 // ======================================================================
 
